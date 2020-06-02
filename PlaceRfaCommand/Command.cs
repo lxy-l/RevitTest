3d32e846eb3a1d00e2f11dafdda39243d0d0910a
Roy <lxysac@foxmail.com>
2020-06-02 10:31:19 +0800

    新增API

--- PlaceRfaCommand/Command.cs
+++ PlaceRfaCommand/Command.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace PlaceRfaCommand
 {
@@ -16,46 +17,59 @@ namespace PlaceRfaCommand
     [Regeneration(RegenerationOption.Manual)]
     public class Command : IExternalCommand
     {
-        private readonly static string rfaPath = $@"{System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}\爱奥尼柱.rfa";
+        private static string rfaPath = $@"{System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}\爱奥尼柱.rfa";
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
             UIApplication app = commandData.Application;
             Document doc = app.ActiveUIDocument.Document;
-            if (!File.Exists(rfaPath))
-            {
-                message = "族不存在!";
-                return Result.Failed;
+            //if (!File.Exists(rfaPath))
+            //{
+            //    message = "族不存在!";
+            //    return Result.Failed;
 
-            }
-            Document familyDoc = doc.Application.OpenDocumentFile(rfaPath);
-            FamilySymbol familySymbol=null;
-            using (Transaction doctran = new Transaction(doc, "Load"))
+            //}
+            Document familyDoc = null;
+            FamilySymbol familySymbol = null;
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Title = "请选择文件";
+            dialog.Filter = "族文件(*.rfa)|*.rfa";
+            if (dialog.ShowDialog() == DialogResult.OK)
             {
-                doctran.Start();
-                if (doc.LoadFamily(rfaPath, out Family family))
+                rfaPath = dialog.FileName;
+                familyDoc = doc.Application.OpenDocumentFile(dialog.FileName);
+                using (Transaction doctran = new Transaction(doc, "Load"))
                 {
-                    doctran.Commit();
-                    familySymbol = doc.GetElement(family.GetFamilySymbolIds().First()) as FamilySymbol;
-                }
-                else
-                {
-                    familySymbol = new FilteredElementCollector(doc).OfClass(typeof(FamilySymbol)).Select(p => p as FamilySymbol).FirstOrDefault(p => p.Name == familyDoc.Title.Replace(".rfa", ""));
-                }
-                if (familySymbol != null)
-                {
-                    if (doctran.GetStatus()!=TransactionStatus.Started)
+                    doctran.Start();
+                    if (doc.LoadFamily(rfaPath, out Family family))
                     {
-                        doctran.Start();
+                        doctran.Commit();
+                        var ids = family.GetTypeId();
+                        familySymbol = doc.GetElement(family.GetFamilySymbolIds().First()) as FamilySymbol;
                     }
-                    familySymbol.Activate();
-                    RevitCommandId commandId = RevitCommandId.LookupCommandId("ID_OBJECTS_FAMSYM");
-                    if (app.CanPostCommand(commandId))
+                    else
                     {
-                        app.PostCommand(commandId);
+                        familySymbol = new FilteredElementCollector(doc).OfClass(typeof(FamilySymbol))
+                            .Select(p => p as FamilySymbol)
+                            .FirstOrDefault(p => p.Name == familyDoc.Title.Replace(".rfa", ""));
                     }
-                    doctran.Commit();
+                    //&&familySymbol.Category.CategoryType!= CategoryType.Annotation
+                    if (familySymbol != null)
+                    {
+                        if (doctran.GetStatus() != TransactionStatus.Started)
+                        {
+                            doctran.Start();
+                        }
+                        familySymbol.Activate();
+                        RevitCommandId commandId = RevitCommandId.LookupCommandId("ID_OBJECTS_FAMSYM");
+                        if (app.CanPostCommand(commandId))
+                        {
+                            app.PostCommand(commandId);
+                        }
+                        doctran.Commit();
+                    }
                 }
-            }        
+            }
+            
             return Result.Succeeded;
         }
         private class projectFamLoadOption : IFamilyLoadOptions
